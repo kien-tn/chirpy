f83caf58f9dccfab7d259985ef5b53b684a863e5
Kien.Nguyen <kien.nguyen@vipps.no>
2025-03-20 22:56:20 +0100

    middleware count

--- main.go
+++ main.go
@@ -1,11 +1,52 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
+	"strconv"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Increment the fileserverHits counter
+		cfg.fileserverHits.Add(1)
+
+		// Print the current hit count to stdout
+		fmt.Fprintln(os.Stdout, "Hitting:", cfg.fileserverHits.Load())
+
+		// Call the next handler in the chain
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
+	apiCfg := &apiConfig{}
+	fmt.Fprintln(os.Stdout, "Hitting:", apiCfg.fileserverHits.Load())
 	mux := http.NewServeMux()
+	mux.Handle("/app/", http.StripPrefix("/app", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(".")))))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		// ContentType
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+	mux.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
+		// ContentType
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Hits: " + strconv.Itoa(int(apiCfg.fileserverHits.Load()))))
+	})
+	mux.HandleFunc("/reset", func(w http.ResponseWriter, r *http.Request) {
+		apiCfg.fileserverHits.Store(0)
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
